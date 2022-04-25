f72a56c54a456926d200a7370fdbb600cbbf9f31
Katie <99366154+kaitlingreen@users.noreply.github.com>
2022-04-25 03:57:26 +0000

    little spelling error

--- results.php
+++ results.php
@@ -2,5 +2,5 @@
   $fahrenheit = $_POST["fahrenheit"];
   $celsius = (5/9) * ($fahrenheit - 32);
 ?>
-<h5>The fahrenheit has been converted successfuly into Celsius and will now be displayed in this section.</h5>
+<h5>The fahrenheit has been converted successfully into Celsius and will now be displayed in this section.</h5>
 <?php echo "the temperature unit Fahrenheit that was provided converted into the temperature unit Celsius, rounded to two decimal places, is " . round($celsius, 2) . " cm<sup>°</sup>" ?>
