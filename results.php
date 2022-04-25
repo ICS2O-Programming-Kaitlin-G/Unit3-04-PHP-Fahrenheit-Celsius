fb4ae6ad38864f398f93d055a2d2ad2e30bfc62a
Katie <99366154+kaitlingreen@users.noreply.github.com>
2022-04-25 02:02:18 +0000

    an error has been fixed. rejoice.

--- results.php
+++ results.php
@@ -1,6 +1,6 @@
 <?php
-  $fahrenheit = $_POST["fahrenheit"]
-  $celsius = (5/9) * ($fahrenheit - 32)
+  $fahrenheit = $_POST["fahrenheit"];
+  $celsius = (5/9) * ($fahrenheit - 32);
 ?>
 <h5>The fahrenheit has been converted successfuly into Celsius and will now be displayed in this section.</h5>
-<?php echo "the temperature unit Fahrenheit that was provided converted into the temperature unit Celsius, rounded to two decimal places, is " . round($celsius, 2) . " cm<sup>°</sup> :)" ?>
+<?php echo "the temperature unit Fahrenheit that was provided converted into the temperature unit Celsius, rounded to two decimal places, is " . round($celsius, 2) . " cm<sup>°</sup>" ?>
